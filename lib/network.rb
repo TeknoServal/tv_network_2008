e4c3edafb5248002e31b6381da245df72a9f4b69
Aidan Murray <aidanhansm@gmail.com>
2020-09-01 11:23:02 -0600

    Add method for adding a show

--- lib/network.rb
+++ lib/network.rb
@@ -3,8 +3,13 @@
 # Network class
 class Network
   attr_reader :name, :shows
+
   def initialize(name)
     @name = name
     @shows = []
+  end
+
+  def add_show(show)
+    @shows << show
   end
 end
