c02498013d389bf5c059b83559227f50b3449941
Aidan Murray <aidanhansm@gmail.com>
2020-09-01 10:47:51 -0600

    Add character class and attributes

---
+++ lib/character.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Character class
+class Character
+  def initialize(data)
+    @name == data[name]
+    @actor = data[actor]
+    @salary = data[salary]
+  end
+end
