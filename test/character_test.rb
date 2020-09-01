bfc419fe28a42910e12c74b2f0abe99989bfec06
Aidan Murray <aidanhansm@gmail.com>
2020-09-01 10:47:25 -0600

    Add test for character and attributes

---
+++ test/character_test.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+
+class CharacterTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    kitt = Character.new({ name: 'KITT', actor: 'William Daniels', salary: 1_000_000 })
+
+    assert_instance_of Character, kitt
+
+    assert_equal 'KITT', kitt.name
+    assert_equal 'William Daniels', kitt.actor
+    assert_equal 1_000_000, kitt.salary
+  end
+end
