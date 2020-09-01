6a54197419c9f0c4ae6cce71e6eec6aba39990d4
Aidan Murray <aidanhansm@gmail.com>
2020-09-01 10:50:18 -0600

    Fix character class

--- lib/character.rb
+++ lib/character.rb
@@ -2,9 +2,10 @@
 
 # Character class
 class Character
+  attr_reader :name, :actor, :salary
   def initialize(data)
-    @name == data[name]
-    @actor = data[actor]
-    @salary = data[salary]
+    @name = data[:name]
+    @actor = data[:actor]
+    @salary = data[:salary]
   end
 end
